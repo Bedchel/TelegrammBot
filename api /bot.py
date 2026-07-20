a4af13318b1003ce4b4bc0da6b5e04dadc27dcce
Bedchel <grishaev.ma12@gmail.com>
2026-07-20 15:08:32 +0200

    Refactor bot message sending logic and update chats
    Updated chat list and modified message sending logic to remove the infinite loop, reducing wait time between messages.

--- api /bot.py
+++ api /bot.py
@@ -3,23 +3,22 @@ from telethon import TelegramClient
 
 api_id = 37587197
 api_hash = 'ebe579cce7e826af00b4771f6837908d'
-CHATS = ['@deltarune_cases_bot']
+
+# Указали правильные названия чатов
+CHATS = ['@deltarune_cases_bot', 'Ферма tanatolii']
 COMMAND = "/open DELTARUNE"
 
 async def main():
+    # Файлы лежат в папке api, поэтому путь 'api/my_session'
     async with TelegramClient('api/my_session', api_id, api_hash) as client:
-        while True:  # Бесконечный цикл
-            print("Запуск круга отправки...")
-            for chat in CHATS:
-                try:
-                    await client.send_message(chat, COMMAND)
-                    print(f"Сообщение успешно отправлено в {chat}")
-                    await asyncio.sleep(5)
-                except Exception as e:
-                    print(f"Не удалось отправить в {chat}: {e}")
-            
-            print("Круг завершен. Ждем 30 минут...")
-            await asyncio.sleep(1800)  # Пауза 30 минут перед следующим кругом
+        print("Запуск отправки...")
+        for chat in CHATS:
+            try:
+                await client.send_message(chat, COMMAND)
+                print(f"Сообщение успешно отправлено в {chat}")
+                await asyncio.sleep(3)
+            except Exception as e:
+                print(f"Не удалось отправить в {chat}: {e}")
 
 if __name__ == '__main__':
     asyncio.run(main())
