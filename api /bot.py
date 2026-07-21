6a123bdc7a672e4e40638d705261ecf8b02ac9b6
Bedchel <grishaev.ma12@gmail.com>
2026-07-21 06:51:30 +0200

    Update CHATS list and sleep duration in bot.py
    Removed one chat from the CHATS list and adjusted sleep duration.

--- api /bot.py
+++ api /bot.py
@@ -6,7 +6,7 @@ api_id = 37587197
 api_hash = 'ebe579cce7e826af00b4771f6837908d'
 
 # Оба чата: первый бот и твоя ферма
-CHATS = ['@deltarune_cases_bot', 'Ферма tanatolii']
+CHATS = ['@deltarune_cases_bot']
 COMMAND = "/open DELTARUNE"
 
 async def main():
@@ -27,7 +27,7 @@ async def main():
                     print(f"Не удалось отправить в {chat}: {e}")
             
             print("Круг завершен. Засыпаем на 30 минут...")
-            await asyncio.sleep(1800) # Спим ровно 30 минут (1800 секунд)
+            await asyncio.sleep(18) # Спим ровно 30 минут (1800 секунд)
 
 if __name__ == '__main__':
     asyncio.run(main())
